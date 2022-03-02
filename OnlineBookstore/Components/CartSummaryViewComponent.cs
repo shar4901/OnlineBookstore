22f7a413ae8185cac5caaf1fe367401e9014f228
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-03-01 23:27:22 -0700

    Shopping cart

---
+++ OnlineBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+
+namespace OnlineBookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
